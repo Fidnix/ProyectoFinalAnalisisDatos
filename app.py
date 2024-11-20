55e6a284144dffa4b69de58d2b270ca44e0cce2f
Fidnix <fidel.moises0@gmail.com>
2024-11-20 13:00:42 -0500

    Nueva correccion de app

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 import streamlit as st
-from st_pages import add_page_title, get_nav_from_toml
 
 # IMPORANTE
 # Este archivo es para configurar la navegación en el sitio web, usa "pages" para guardar archivos .py
 # Y configura nuevas páginas en pages.toml
-
-# st.set_page_config(layout="wide")
-# nav = get_nav_from_toml("pages.toml")
-
-# pg = st.navigation(nav)
-# add_page_title(pg)
-# pg.run()
 
 pages = [
     st.Page("pages/intro.py", title="Introducción"),
